fb9732acf7f005c0ba6c85ee8b93081e0244c5c7
Otavio <ottvferreira@gmail.com>
2022-07-09 00:32:14 -0300

    Utilizando de classe invólucro para atribuir valor inteiro a uma string

--- teste-tipos/TesteTipos/src/testetipos/TesteTipos.java
+++ teste-tipos/TesteTipos/src/testetipos/TesteTipos.java
@@ -16,7 +16,8 @@ public class TesteTipos {
     public static void main(String[] args) {
         // TODO code application logic here
         int idade = 30;
-        String valor = idade;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
     }
     
 }
