1339623500e824b819c377c1b074ae0e2a730cae
Otavio <ottvferreira@gmail.com>
2022-07-09 00:23:43 -0300

    Trabalhando com leitura e visualização de dados no terminal

--- tipos-primitivos/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ tipos-primitivos/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -4,6 +4,9 @@
  */
 package tiposprimitivos;
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author otvfe
@@ -15,6 +18,12 @@ public class TiposPrimitivos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.println("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        System.out.format("A nota de %s é %.2f \n", nome, nota);
     }
     
 }
