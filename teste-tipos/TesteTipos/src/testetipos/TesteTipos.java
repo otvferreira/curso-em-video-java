1c28c291c3a31bd8b3454225684491dea55ebd4a
Otavio <ottvferreira@gmail.com>
2022-07-11 19:24:04 -0300

    Incompatibilidades entre tipos

--- teste-tipos/TesteTipos/src/testetipos/TesteTipos.java
+++ teste-tipos/TesteTipos/src/testetipos/TesteTipos.java
@@ -14,10 +14,12 @@ public class TesteTipos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        int idade = 30;
+        /*int idade = 30;
         String valor = Integer.toString(idade);
-        System.out.println(valor);
+        System.out.println(valor);*/
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
     }
     
 }
--- tipos-primitivos/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ tipos-primitivos/TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tiposprimitivos;
 
 import java.util.Scanner;
@@ -9,13 +6,11 @@ import java.util.Scanner;
 
 /**
  *
- * @author otvfe
+ * @author otvferreira
  */
 public class TiposPrimitivos {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado = new Scanner(System.in);
